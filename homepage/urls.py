2bd64e98c96772c6650414aa68f76684ea48b998
abdeel94 <abdeel4991@hotmail.com>
2022-06-07 09:57:21 -0400

    cambio url

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,7 +3,7 @@ from .views import home, forma_producto, modificar_producto, eliminar_producto, 
 
 urlpatterns = [
     path('', home ,name="home"),
-    path('forma_producto', forma_producto, name="forma_vehiculo"),
+    path('forma_producto', forma_producto, name="forma_producto"),
     path('modificar_producto/<id>', modificar_producto, name="modificar_producto"),
     path('eliminar_producto/<id>', eliminar_producto, name="eliminar_producto"),
     path('lista_mod', lista_mod ,name="lista_mod"),
