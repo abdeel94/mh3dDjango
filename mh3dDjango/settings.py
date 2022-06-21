ead433c4ce958b86688efabd16529dc7cd9da55b
abdeel94 <abdeel4991@hotmail.com>
2022-06-21 15:25:32 -0400

    token security

--- mh3dDjango/settings.py
+++ mh3dDjango/settings.py
@@ -44,7 +44,14 @@ INSTALLED_APPS = [
     'crispy_forms',
     'rest_producto',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 # CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
--- rest_producto/views.py
+++ rest_producto/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from rest_framework import status
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from homepage.models import Producto
 from rest_producto.serializers import ProductoSerializer
-# Create your views here.
+
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 @csrf_exempt
 @api_view(['GET','POST'])
+@permission_classes((IsAuthenticated,))
 def lista_productos(request):
     if request.method == 'GET':
         listaProductos = Producto.objects.all()
@@ -26,6 +30,7 @@ def lista_productos(request):
         
 @csrf_exempt
 @api_view(['GET','PUT','DELETE'])
+@permission_classes((IsAuthenticated,))
 def detalle_producto(request, id):
     try:
         producto = Producto.objects.get(idProducto=id)
