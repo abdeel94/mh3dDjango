22a26979ebc7f4ec939735de8f5f3041a68e2b5c
abdeel94 <abdeel4991@hotmail.com>
2022-06-25 17:06:38 -0400

    fixes frontend

--- homepage/forms.py
+++ homepage/forms.py
@@ -1,20 +1,31 @@
+from logging import PlaceHolder
 from django import forms
 from django.forms import ModelForm
 from .models import Producto
 from allauth.account.forms import SignupForm
 
+
 class ProductoForm(ModelForm):
-    
+
     class Meta:
         model = Producto
-        fields = ['nombreProducto','descripcionProducto','categoria','imagenProducto']
-        
-        
+        fields = [
+            'nombreProducto', 'descripcionProducto', 'categoria',
+            'imagenProducto'
+        ]
+
+
 class MyCustomSignupForm(SignupForm):
-    
-    first_name = forms.CharField(max_length=30, label='Nombre')
-    last_name = forms.CharField(max_length=30, label='Apellido')
- 
+
+    first_name = forms.CharField(
+        max_length=30,
+        label='Nombre',
+        widget=forms.TextInput(attrs={'placeholder': 'Nombre'}))
+    last_name = forms.CharField(
+        max_length=30,
+        label='Apellido',
+        widget=forms.TextInput(attrs={'placeholder': 'Apellido'}))
+
     def save(self, request):
         user = super(MyCustomSignupForm, self).save(request)
         user.first_name = self.cleaned_data['first_name']
